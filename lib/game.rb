f3c34d0709a3a34366682e81c2c2e49caae57f92
Vadim A. Venediktov <install.vv@gmail.com>
2016-08-27 15:09:09 +0300

    COMMENTS

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,18 @@
 # encoding: utf-8
 
-# Основной класс игры. Хранит состояние игры и предоставляет функции
-# для развития игры (ввод новых букв, подсчет кол-ва ошибок и т. п.)
-#
-# С поддержко русских букв в верхнем и нижнем регистрах
-
 # Подключаем библиотеку для обработки (в т.ч.) русских строк
 require 'unicode'
 
+# Основной класс игры. Хранит состояние игры и предоставляет функции
+# для развития игры (ввод новых букв, подсчет кол-ва ошибок и т. п.)
 class Game
+  # Сокращенный способ записать сеттеры для получения информации об игре
   attr_reader :errors, :letters, :good_letters, :bad_letters, :status
 
+  # Сокращенный способ записать и сеттер и геттер для поля version
   attr_accessor :version
 
+  # Константа с максимальным количеством ошибок
   MAX_ERRORS = 7
 
   def initialize(slovo)
@@ -23,6 +23,8 @@ class Game
     @good_letters = []
     @bad_letters = []
 
+    # В поле @status теперь будет лежать не бездушная цифра
+    # А символ, который наглядно показывает статус
     @status = :in_progress # :won, :lost
   end
 
@@ -36,14 +38,17 @@ class Game
     Unicode.upcase(slovo).split('')
   end
 
+  # Метод, который просто возвращает константу MAX_ERRORS
   def max_errors
     MAX_ERRORS
   end
 
+  # Метод, который возвращает количество оставшихся ошибок
   def errors_left
     MAX_ERRORS - @errors
   end
 
+  # Метод, который отвечает на вопрос, является ли буква подходящей
   def is_good?(letter)
     @letters.include?(letter) ||
       (letter == "Е" && @letters.include?("Ё")) ||
@@ -52,7 +57,11 @@ class Game
       (letter == "Й" && @letters.include?("И"))
   end
 
+  # Метод добавляет букву к массиву (хороших или плохих букв)
   def add_letter_to(letters, letter)
+    # Обратите внимание, что переменная - это только ярлык,
+    # не смотря на то, что letters после работы метода исчезнет,
+    # объект, который мы поменяли, останется
     letters << letter
 
     case letter
@@ -63,45 +72,61 @@ class Game
     end
   end
 
+  # Метод, который отвечает на вопрос, отгадано ли все слово
   def solved?
     (@letters - @good_letters).empty?
   end
 
+  # Метод, который отвечает на вопрос, была ли уже эта буква
   def repeated?(letter)
     @good_letters.include?(letter) || @bad_letters.include?(letter)
   end
 
+  # Метод, который отвечает на вопрос, проиграна ли игра
   def lost?
     @status == :lost || @errors >= MAX_ERRORS
   end
 
+  # Метод, который отвечает на вопрос, продолжается ли игра
   def in_progress?
     @status == :in_progress
   end
 
+  # Метод, который отвечает на вопрос, выиграл ли игрок
   def won?
     @status == :won
   end
 
+  # Старый метод, который продвигает состояние игры на следующий ход
   def next_step(letter)
+    # Поднимаем букву в верхний регистр
     letter = Unicode.upcase(letter)
 
+    # Вываливаемся, если игра уже закончена
     return if @status == :lost || @status == :won
+
+    # Вываливаемся, если буква уже была
     return if repeated?(letter)
 
     if is_good?(letter)
+      # Если буква подошла, добавляем её к хорошим
       add_letter_to(@good_letters, letter)
 
+      # Если слово отгадано, меняем статус
       @status = :won if solved?
     else
+      # Если буква не подошла, добавляем к плохим
       add_letter_to(@bad_letters, letter)
 
+      # Увеличиваем количество ошибок
       @errors += 1
 
+      # Меняем статус на проигрыш, еслиигра проиграна
       @status = :lost if lost?
     end
   end
 
+  # Метод, который спрашивает у пользователя следующую букву
   def ask_next_letter
     puts "\nВведите следующую букву"
     letter = ""
--- viselitsa.rb
+++ viselitsa.rb
@@ -9,17 +9,23 @@ require current_path + "/lib/game.rb"
 require current_path + "/lib/result_printer.rb"
 require current_path + "/lib/word_reader.rb"
 
+# Записываем версию игры в константу VERSION
 VERSION = "Игра виселица, версия 4. (c) Хороший программист\n"
 
+# Создаем экземпляр класса WordReader
 word_reader = WordReader.new
 words_file_name = current_path + "/data/words.txt"
 word = word_reader.read_from_file(words_file_name)
 
+# Создаем игру и прописываем ее версию с помощью сеттера version=
 game = Game.new(word)
 game.version = VERSION
 
+# Теперь экземпляр ResultPrinter-а нельзя создать без игры
+# Именно поэтому порядо создания методов именно такой
 printer = ResultPrinter.new(game)
 
+# Основной игровой цикл остался прежним
 while game.in_progress? do
   printer.print_status(game)
   game.ask_next_letter
